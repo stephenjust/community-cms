fe65a2d03f4a63fbf075b7e97f5bd5bf78820406
Stephen Just <stephenjust@users.sf.net>
2008-10-16 00:18:08 +0000

    It felt like a good idea to put some of the user information in a constant. Might be useful later.

--- functions/login.php
+++ functions/login.php
@@ -23,6 +23,7 @@
 				$_SESSION['pass'] = md5($passwd);
 				$_SESSION['name'] = $result['realname'];
 				$_SESSION['type'] = $result['type'];
+				define('USERINFO',$result['id'].','.$result['realname'].','.$result['type']);
 				}
 			}
 		}
@@ -38,15 +39,21 @@
 		global $CONFIG;
 		global $db;
 		if(isset($_SESSION['user'])) {
-			$query = 'SELECT username,password,realname,type FROM '.$CONFIG['db_prefix'].'users WHERE username = \''.$_SESSION['user'].'\' AND password = \''.$_SESSION['pass'].'\' AND type = \''.$_SESSION['type'].'\' AND realname = \''.$_SESSION['name'].'\'';
+			$query = 'SELECT id,username,password,realname,type FROM '.$CONFIG['db_prefix'].'users WHERE username = \''.$_SESSION['user'].'\' AND password = \''.$_SESSION['pass'].'\' AND type = \''.$_SESSION['type'].'\' AND realname = \''.$_SESSION['name'].'\'';
 			$access = $db->query($query);
 			$num_rows = $access->num_rows;
 		  if($num_rows != 1) {
 				logout();
 				err_page(3002);
+				return;
 				}
-			} elseif($mustbeloggedin == 1) {
+			$userinfo = $access->fetch_assoc();
+			define('USERINFO',$userinfo['id'].','.$userinfo['realname'].','.$userinfo['type']);
+			return;
+			}
+		if($mustbeloggedin == 1) {
 			err_page(3004);
+			return;
 			}
 		}
 	function checkuser_admin() {
@@ -61,6 +68,9 @@
 	  if($num_rows != 1) {
 			logout();
 			err_page(3002);
+			return;
 			}
+			$userinfo = $access->fetch_assoc();
+			define('USERINFO',$userinfo['id'].','.$userinfo['realname'].','.$userinfo['type']);
 		}
 ?>
