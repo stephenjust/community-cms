cec4044c10ac873a3088205d80b3412dd5225775
Stephen Just <stephenjust@users.sf.net>
2008-12-31 01:13:25 +0000

    Do not cache the page if you are sending a message.

--- pagetypes/contacts.php
+++ pagetypes/contacts.php
@@ -22,6 +22,11 @@
 		$_GET['action'] = '';
 		}
 	if($_GET['message'] != '') {
+		header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+		header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); // always modified
+		header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0"); // HTTP/1.1
+		header("Cache-Control: post-check=0, pre-check=0", false);
+		header("Pragma: no-cache"); // HTTP/1.0
 		$content .= '<form method="POST" action="index.php?id='.$_GET['id'].'&action=send">
 <input type="hidden" name="recipient" value="'.$_GET['message'].'" />
 Message to user:<br />
