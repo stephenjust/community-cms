1b93ffc17f21cfe03826ecbcffbb7fa1c8b318ce
Stephen Just <stephenjust@users.sf.net>
2009-02-13 03:33:53 +0000

    Use forms instead of JavaScript for minipoll voting. Fixes bug in IE7.

--- functions/pages.php
+++ functions/pages.php
@@ -11,6 +11,18 @@
 		global $CONFIG;
 		global $db;
 		global $NOTIFICATION;
+		if(isset($_POST['vote']) && isset($_POST['vote_poll'])) {
+			$question_id = $_POST['vote_poll'];
+			$answer_id = $_POST['vote'];
+			$user_ip = $_SERVER['REMOTE_ADDR'];
+			$query = 'INSERT INTO '.$CONFIG['db_prefix'].'poll_responses (question_id ,answer_id ,value ,ip_addr) VALUES ('.$question_id.', '.$answer_id.', NULL, \''.ip2long($user_ip).'\');';
+			$handle = $db->query($query);
+			if(!$handle) {
+				echo('Failed to submit your vote.');
+				} else {
+				echo('Thank you for voting.');
+				}
+			}
 		$page_type_query = 'SELECT * FROM '.$CONFIG['db_prefix'].'pagetypes WHERE id = '.$type.' LIMIT 1';
 		$page_type_handle = $db->query($page_type_query);
 		try {
