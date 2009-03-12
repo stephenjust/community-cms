81808d654820f210d35c82fb51f00f83023cd23d
Stephen Just <stephenjust@users.sf.net>
2009-03-12 01:30:33 +0000

    Removed TinyMCE media module.

--- admin.php
+++ admin.php
@@ -64,7 +64,7 @@
 <script language="javascript" type="text/javascript" src="./admin/scripts/block_options.js"></script>
 <script type="text/javascript">
 tinyMCE_GZ.init({
-	plugins : \'style,layer,table,save,advhr,advimage,advlink,iespell,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,spellchecker,noneditable,visualchars,nonbreaking,xhtmlxtras\',
+	plugins : \'style,layer,table,save,advhr,advimage,advlink,iespell,insertdatetime,preview,searchreplace,print,contextmenu,paste,directionality,fullscreen,spellchecker,noneditable,visualchars,nonbreaking,xhtmlxtras\',
 	themes : "advanced",
 	languages : "en",
 	disk_cache : true,
@@ -77,10 +77,10 @@ tinyMCE.init({
 	editor_deselector : "mceNoEditor",
 	theme : "advanced",
 	skin : "o2k7",
-	plugins : "style,layer,table,save,advhr,advimage,advlink,iespell,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,spellchecker,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras",
+	plugins : "style,layer,table,save,advhr,advimage,advlink,iespell,insertdatetime,preview,searchreplace,print,contextmenu,paste,directionality,spellchecker,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras",
 	theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontselect,fontsizeselect,|,help,code",
 	theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,image,cleanup,|,forecolor,backcolor,|,spellchecker",
-	theme_advanced_buttons3 : "tablecontrols,|,sub,sup,|,charmap,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
+	theme_advanced_buttons3 : "tablecontrols,|,sub,sup,|,charmap,iespell,advhr,|,print,|,ltr,rtl,|,fullscreen",
 	theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,|,visualchars,nonbreaking,|,insertdate,inserttime,preview,|,hr,removeformat,visualaid",
 	theme_advanced_toolbar_location : "top",
 	theme_advanced_toolbar_align : "left",
