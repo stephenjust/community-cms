a61b59c7366b9d812e89f25583a0d8ef9a8288be
Stephen Just <stephenjust@users.sf.net>
2009-03-29 00:01:26 +0000

    Added ability to clear log messages.

--- admin/log_view.php
+++ admin/log_view.php
@@ -4,6 +4,19 @@
 		die ('You cannot access this page directly.');
 		}
 	$content = NULL;
+	if($_GET['action'] == 'delete') {
+		$delete_query = 'TRUNCATE TABLE '.$CONFIG['db_prefix'].'logs';
+		$delete_handle = $db->query($delete_query);
+		if(!$delete_handle) {
+			$content .= 'Failed to clear log messages.<br />';
+			} else {
+			$content .= 'Cleared log messages.';
+			}
+		log_action('Cleared log messages.');
+		}
+
+// ----------------------------------------------------------------------------
+
 	$log_message_query = 'SELECT * FROM '.$CONFIG['db_prefix'].'logs log, '.$CONFIG['db_prefix'].'users user WHERE log.user_id = user.id ORDER BY log.date DESC';
 	$log_message_handle = $db->query($log_message_query);
 	if(!$log_message_handle) {
@@ -36,5 +49,8 @@ $rowtype = 1;
 			}
 		$i++;
 		}
-	$content .= '</table>';
+	$content .= '</table>
+<form method="post" action="admin.php?module=log_view&action=delete">
+<input type="submit" value="Clear Log Messages" />
+</form>';
 ?>
