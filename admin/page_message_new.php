746f078f83e18373136d46d07e90436b94bd650d
Stephen Just <stephenjust@users.sf.net>
2009-05-12 03:14:53 +0000

    Use form class for new page messages.

--- admin/page_message_new.php
+++ admin/page_message_new.php
@@ -20,7 +20,7 @@ if ($_GET['action'] == 'save') {
 		$text = addslashes($_POST['text']);
 		$new_message_query = 'INSERT INTO ' . PAGE_MESSAGE_TABLE . "
 			SET start_date='$start_date',end_date='$end_date',end='$expire',
-			text='$text',page_id='$page_id'";
+			text='$text',page_id='$page_id',`order`='0'";
 		$new_message = $db->sql_query($new_message_query);
 		if ($db->error[$new_message] === 1) {
 			$content .= 'Failed to create page message.<br />';
@@ -35,42 +35,16 @@ if ($_GET['action'] == 'save') {
 	if (!isset($_GET['page']) || $_GET['page'] == '') {
 		$_GET['page'] = 1;
 	}
-	$content = '<form method="POST" action="admin.php?module=page_message_new&action=save">
-		<h1>Create Page Message</h1>
-		<table class="admintable">
-		<input type="hidden" name="page_id" value="'.$_GET['page'].'" />
-		<tr><td class="row1" valign="top">Content:</td><td class="row1"><textarea name="text" rows="30"></textarea></td></tr>
-		<tr><td width="150" class="row2" valign="top">Date:</td><td class="row2">';
-	$months = array('January','February','March','April','May','June','July',
-		'August','September','October','November','December');
-	$content .= 'Start:<br />';
-	$content .= '<select name="start_month">';
-	$monthcount = 1;
-	while ($monthcount <= 12) {
-		if(date('m') == $monthcount) {
-			$content .= "<option value='".$monthcount."' selected >".$months[$monthcount-1]."</option>"; // Need [$monthcount-1] as arrays start at 0.
-			$monthcount++;
-		} else {
-			$content .= "<option value='".$monthcount."'>".$months[$monthcount-1]."</option>";
-			$monthcount++;
-		}
-	}
-	$content .= '</select><input type="text" name="start_day" maxlength="2" size="2" value="'.date('d').'" /><input type="text" name="start_year" maxlength="4" size="4" value="'.date('Y').'" /><br />
-		End:<br />';
-	$content .= '<select name="end_month">';
-	$monthcount = 1;
-	while ($monthcount <= 12) {
-		if(date('m') == $monthcount) {
-			$content .= "<option value='".$monthcount."' selected >".$months[$monthcount-1]."</option>"; // Need [$monthcount-1] as arrays start at 0.
-			$monthcount++;
-		} else {
-			$content .= "<option value='".$monthcount."'>".$months[$monthcount-1]."</option>";
-			$monthcount++;
-		}
-	}
-	$content .= '</select><input type="text" name="end_day" maxlength="2" size="2" value="'.date('d').'" /><input type="text" name="end_year" maxlength="4" size="4" value="'.date('Y').'" /></td></tr>
-		<tr><td width="150" class="row1">Expire:</td><td class="row1"><input type="checkbox" name="expire" /></td></tr>
-		<tr><td width="150" class="row2">&nbsp;</td><td class="row2"><input type="submit" value="Submit" /></td></tr>
-		</table>';
+	$content = NULL;
+	$form = new form;
+	$form->set_target('admin.php?module=page_message_new&amp;action=save');
+	$form->set_method('post');
+	$form->add_hidden('page_id',(int)$_GET['page']);
+	$form->add_textarea('text','Content',NULL,'rows="30"');
+	$form->add_date('start','Start Date','MDY');
+	$form->add_date('end','End Date','MDY');
+	$form->add_checkbox('expire','Expire');
+	$form->add_submit('submit','Save');
+	$content .= '<h1>Create New Page Message</h1>'.$form;
 }
 ?>
