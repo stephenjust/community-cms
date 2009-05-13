a86b9b469490c312732497e47a81ab7b3665ddbe
Stephen Just <stephenjust@users.sf.net>
2009-05-13 01:49:26 +0000

    Fixed minor coding style problems, removed unnecessary inclusion of $CONFIG.

--- functions/blocks.php
+++ functions/blocks.php
@@ -3,9 +3,9 @@
 if (@SECURITY != 1) {
 	die ('You cannot access this page directly.');
 }
+
 /**
  * get_block - Get contents of a block
- * @global array $CONFIG
  * @global resource $db
  * @param int $block_id ID of block to display
  * @return string
@@ -15,14 +15,13 @@ function get_block($block_id = NULL) {
 	if(strlen($block_id) < 1 || $block_id <= 0) {
 		return;
 	}
-	global $CONFIG;
 	global $db;
 	$block_content = NULL;
 	$block_query = 'SELECT * FROM ' . BLOCK_TABLE . '
 		WHERE id = '.$block_id.' LIMIT 1';
 	$block_handle = $db->sql_query($block_query);
-	if($db->error[$block_handle] === 0) {
-		if($db->sql_num_rows($block_handle) == 1) {
+	if ($db->error[$block_handle] === 0) {
+		if ($db->sql_num_rows($block_handle) == 1) {
 			$block_info = $db->sql_fetch_assoc($block_handle);
 			$block_content .= include(ROOT.'content_blocks/'.$block_info['type'].'_block.php');
 		} else {
