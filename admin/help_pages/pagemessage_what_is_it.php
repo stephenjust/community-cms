78eeabd907b5bb284578b3044fb8ecae49b7a3e9
Stephen Just <stephenjust@users.sf.net>
2009-05-16 21:22:03 +0000

    Added help page with explanation of page messages.

---
+++ admin/help_pages/pagemessage_what_is_it.php
@@ -0,0 +1,38 @@
+<?php
+// Security Check
+if (@SECURITY != 1 || @ADMIN != 1) {
+	header('HTTP/1.1 403 Forbidden');
+	die ('<html>
+<head>
+<title>Forbidden</title>
+</head>
+<body>
+You cannot access this page directly. Please view<br />
+this help file through the administrative interface<br />
+by clicking the question mark in the top right corner<br />
+of any administration page.
+</body>
+</html>');
+}
+// Back to table of contents
+$return = '<br /><a href="admin.php?module=help">Table of Contents</a><br />';
+
+// Page header
+$return .= '<div id="admin_help_page"><h3>What is a Page Message?</h3>';
+
+// Quick overview of what can be done
+$return .= '<div class="admin_help_explanation">Page messages allow you to add
+static content to your pages. A page message appears at the top of a certain page.
+Similar to text blocks, they allow you to add content to the top of a page.</div>';
+
+// Quick instructions
+
+// In-depth instructions
+
+
+$return .= '</div>';
+
+// Back to table of contents
+$return .= '<br /><a href="admin.php?module=help">Table of Contents</a><br />';
+return $return;
+?>
--- admin/help_pages/table_of_contents.php
+++ admin/help_pages/table_of_contents.php
@@ -17,22 +17,23 @@ of any administration page.
 	$return = '<div id="admin_help_toc"><h3>Table of Contents</h3>';
 	$return .= '<h4>News</h4>
 <ol>
-<li><a href="admin.php?module=help&page=news_new_article">Create New Article</a></li>
+<li><a href="admin.php?module=help&amp;page=news_new_article">Create New Article</a></li>
 </ol>';
 	$return .= '<h4>Newsletters</h4>
 <ol>
-<li><a href="admin.php?module=help&page=newsletter_new_newsletter">Creating a Newsletter</a></li>
+<li><a href="admin.php?module=help&amp;page=newsletter_new_newsletter">Creating a Newsletter</a></li>
 </ol>';
 	$return .= '<h4>Pages</h4>
 <ol>
-<li><a href="admin.php?module=help&page=pages_create_page">Creating a New Page</a></li>
-<li><a href="admin.php?module=help&page=pages_add_block">Adding Blocks to a Page</a></li>
+<li><a href="admin.php?module=help&amp;page=pages_create_page">Creating a New Page</a></li>
+<li><a href="admin.php?module=help&amp;page=pages_add_block">Adding Blocks to a Page</a></li>
+<li><a href="admin.php?module=help&amp;page=pagemessage_what_is_it">What is a Page Message?</a></li>
 </ol>';
 	$return .= '<h4>TinyMCE</h4>
 <ol>
-<li><a href="admin.php?module=help&page=tinymce_using_tinymce">Using the TinyMCE Editor</a></li>
-<li><a href="admin.php?module=help&page=tinymce_what_is_it">What is TinyMCE?</a></li>
-<li><a href="admin.php?module=help&page=tinymce_paste_in_firefox">Pasting in Firefox</a></li>
+<li><a href="admin.php?module=help&amp;page=tinymce_using_tinymce">Using the TinyMCE Editor</a></li>
+<li><a href="admin.php?module=help&amp;page=tinymce_what_is_it">What is TinyMCE?</a></li>
+<li><a href="admin.php?module=help&amp;page=tinymce_paste_in_firefox">Pasting in Firefox</a></li>
 </ol>';
 	$return .= '<h4>Help</h4>
 <ol>
