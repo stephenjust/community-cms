381d3286ad1e0dde773f7bb915f47ae0470f9004
Stephen Just <stephenjust@users.sf.net>
2009-05-24 22:12:32 +0000

    Added permission for clearing log messages. Added stripslashes() to log message display code. Limited log page to display 50 messages.

--- admin/log_view.php
+++ admin/log_view.php
@@ -13,20 +13,30 @@ if (@SECURITY != 1 || @ADMIN != 1) {
 }
 
 $content = NULL;
+
+if (!$acl->check_permission('adm_log_view')) {
+	$content .= 'You do not have the necessary permissions to access this module.';
+	return true;
+}
+
 if ($_GET['action'] == 'delete') {
-	$delete_query = 'TRUNCATE TABLE ' . LOG_TABLE;
-	$delete_handle = $db->sql_query($delete_query);
-	if ($db->error[$delete_handle] === 1) {
-		$content .= 'Failed to clear log messages.<br />';
+	if (!$acl->check_permission('log_clear')) {
+		$content .= 'You are not authorized to clear the log.<br />';
 	} else {
-		$content .= 'Cleared log messages.<br />'.log_action('Cleared log messages.');
+		$delete_query = 'TRUNCATE TABLE `' . LOG_TABLE . '`';
+		$delete_handle = $db->sql_query($delete_query);
+		if ($db->error[$delete_handle] === 1) {
+			$content .= 'Failed to clear log messages.<br />';
+		} else {
+			$content .= 'Cleared log messages.<br />'.log_action('Cleared log messages.');
+		}
 	}
 }
 
 // ----------------------------------------------------------------------------
 
-$log_message_query = 'SELECT * FROM ' . LOG_TABLE . ' l, ' . USER_TABLE . ' u
-	WHERE l.user_id = u.id ORDER BY l.date DESC';
+$log_message_query = 'SELECT * FROM `' . LOG_TABLE . '` l, `' . USER_TABLE . '` u
+	WHERE l.user_id = u.id ORDER BY l.date DESC LIMIT 50';
 $log_message_handle = $db->sql_query($log_message_query);
 if ($db->error[$log_message_handle] === 1) {
 	$content .= 'Failed to read log messages.<br />';
@@ -45,8 +55,8 @@ while ($i <= $num_messages) {
 	$log_message = $db->sql_fetch_assoc($log_message_handle);
 	$tab_content['view'] .= '<tr class="row'.$rowtype.'">
 		<td>'.$log_message['date'].'</td>
-		<td>'.$log_message['action'].'</td>
-		<td>'.$log_message['realname'].'</td>
+		<td>'.stripslashes($log_message['action']).'</td>
+		<td>'.stripslashes($log_message['realname']).'</td>
 		<td>'.long2ip($log_message['ip_addr']).'</td>
 		</tr>';
 	if ($rowtype == 1) {
@@ -57,11 +67,15 @@ while ($i <= $num_messages) {
 	$i++;
 }
 $tab_content['view'] .= '</table>';
-$tab_content['delete'] = '<form method="post" action="admin.php?module=log_view&action=delete">
-<input type="submit" value="Clear Log Messages" />
-</form>';
 $tab_layout = new tabs;
 $tab_layout->add_tab('View Log Messages',$tab_content['view']);
-$tab_layout->add_tab('Delete Log Messages',$tab_content['delete']);
+
+if ($acl->check_permission('log_clear')) {
+	$tab_content['delete'] = '<form method="post" action="admin.php?module=log_view&action=delete">
+	<input type="submit" value="Clear Log Messages" />
+	</form>';
+	$tab_layout->add_tab('Delete Log Messages',$tab_content['delete']);
+}
+
 $content .= $tab_layout;
 ?>
