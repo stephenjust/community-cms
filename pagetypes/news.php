1364ad6b4bbb74a70d454daec0adf1873cd0a8fb
Stephen Just <stephenjust@users.sf.net>
2009-06-06 17:37:40 +0000

    Only display configured number of articles on a page. Pagination does not work for some reason.

--- pagetypes/news.php
+++ pagetypes/news.php
@@ -1,60 +1,86 @@
 <?php
-// Security Check
-if (@SECURITY != 1) {
-	die ('You cannot access this page directly.');
+/**
+ * Community CMS
+ * $Id$
+ *
+ * @copyright Copyright (C) 2007-2009 Stephen Just
+ * @author stephenjust@users.sourceforge.net
+ * @package CommunityCMS.main
+ */
+
+/**
+ * @ignore
+ */
+if (!defined('SECURITY')) {
+	exit;
 }
+
 global $page;
-global $site_info;
-global $page_info;
 global $db;
+$return = NULL;
+
+// Load configuration
+$news_config_query = 'SELECT * FROM ' . NEWS_CONFIG_TABLE . ' LIMIT 1';
+$news_config_handle = $db->sql_query($news_config_query);
+if ($db->error[$news_config_handle] === 1) {
+	$page->notification .= 'Could not load news settings.<br />';
+	return $return;
+} elseif ($db->sql_num_rows($news_config_handle) == 0) {
+	$page->notification .= 'There are no news settings available.<br />';
+	return $return;
+}
+$news_config = $db->sql_fetch_assoc($news_config_handle);
+
 include(ROOT.'pagetypes/news_class.php');
 if(!isset($_GET['start']) || $_GET['start'] == "" || $_GET['start'] < 0) {
 	$_GET['start'] = 0;
 }
 $start = (int)$_GET['start'];
 $first_date = NULL;
-$news_query = 'SELECT id FROM ' . NEWS_TABLE . '
-	WHERE page = '.$page->id.' ORDER BY date DESC
-	LIMIT 10 OFFSET '.$start.'';
+$news_query = 'SELECT `id` FROM `' . NEWS_TABLE . '`
+	WHERE `page` = '.$page->id.' ORDER BY `date`,`id` DESC
+	LIMIT '.$news_config['num_articles'].' OFFSET '.$start.'';
 $news_handle = $db->sql_query($news_query);
-$news_num_rows = $db->sql_num_rows($news_handle);
 // Initialize session variable if not initialized to prevent warnings.
-if(!isset($_SESSION['user'])) {
+if (!isset($_SESSION['user'])) {
 	$_SESSION['user'] = NULL;
 }
-$return = NULL;
-if($news_num_rows == 0) {
+if ($db->sql_num_rows($news_handle) == 0) {
 	$return .= 'There are no articles to be displayed.';
-} else {
-	for ($i = 1; $i <= $news_num_rows; $i++) {
-		$news = $db->sql_fetch_assoc($news_handle);
-		$article = new news_item;
-		$article->set_article_id((int)$news['id']);
-		$article->get_article();
-		$last_article_date = $article->date;
-		$return .= $article;
-		unset($article);
-	}
+	return $return;
 }
+
+for ($i = 1; $i <= $db->sql_num_rows($news_handle); $i++) {
+	$news = $db->sql_fetch_assoc($news_handle);
+	$article = new news_item;
+	$article->set_article_id((int)$news['id']);
+	$article->get_article();
+	$last_article_date = $article->date;
+	$return .= $article."\n\n";
+	unset($article);
+}
+
+
+// FIXME: Pagination does not work.
 $last_article_date = 0;
-$news_first_query = 'SELECT date FROM ' . NEWS_TABLE . '
-	WHERE page = '.$page->id.' ORDER BY date DESC LIMIT 1';
+$news_first_query = 'SELECT `id`,`date` FROM `' . NEWS_TABLE . '`
+	WHERE `page` = '.$page->id.' ORDER BY `date`,`id` DESC LIMIT 1';
 $news_first_handle = $db->sql_query($news_first_query);
 $news_first = $db->sql_fetch_assoc($news_first_handle);
-$news_last_query = 'SELECT date FROM ' . NEWS_TABLE . '
-	WHERE page = '.$page->id.' ORDER BY date ASC LIMIT 1';
+$news_last_query = 'SELECT `id`,`date` FROM `' . NEWS_TABLE . '`
+	WHERE `page` = '.$page->id.' ORDER BY `date`,`id` ASC LIMIT 1';
 $news_last_handle = $db->sql_query($news_last_query);
 $news_last = $db->sql_fetch_assoc($news_last_handle);
 $template_pagination = new template;
 $template_pagination->load_file('pagination');
 if($news_first['date'] != $first_date && isset($first_date)) {
-	$prev_start = $start - 10;
+	$prev_start = $start - $news_config['num_articles'];
 	$template_pagination->prev_page = '<a href="index.php?id='.$id.'&start='.$prev_start.'" class="prev_page" id="prev_page">Previous Page</a>';
 } else {
 	$template_pagination->prev_page = '';
 }
 if($news_last['date'] != $last_article_date && $last_article_date != NULL) {
-	$next_start = $start + 10;
+	$next_start = $start + $news_config['num_articles'];
 	$template_pagination->next_page = '<a href="index.php?id='.$id.'&start='.$next_start.'" class="prev_page" id="prev_page">Next Page</a>';
 } else {
 	$template_pagination->next_page = '';
