88c8d3a00c30b61348d2b1f7ebcfdb6a9ce96e1f
Stephen Just <stephenjust@users.sf.net>
2009-06-24 18:07:45 +0000

    Added page_get_info() function, started page_hide() function.

--- includes/page.php
+++ includes/page.php
@@ -8,6 +8,47 @@
  * @package CommunityCMS.main
  */
 
+function page_get_info($id,$fields = array('*')) {
+	global $db;
+	global $debug;
+	// Validate parameters
+	if (!is_int($id)) {
+		$debug->add_trace('ID is not an integer',true,'page_get_info');
+		return false;
+	}
+	if(!is_array($fields)) {
+		$debug->add_trace('Field list is not an array',true,'page_get_info');
+		return false;
+	}
+	// Add backticks to field names and ensure that there are no spaces in field names
+	$field_count = count($fields);
+	for ($i = 0; $i < $field_count; $i++) {
+		if (eregi(' ',$fields[$i])) {
+			$debug->add_trace('Removed field "'.$fields[$i].'"',false,'page_get_info');
+			unset($fields[$i]);
+			continue;
+		}
+		if ($fields[$i] != '*' && strlen($fields[$i]) > 0) {
+			$fields[$i] = '`'.$fields[$i].'`';
+		}
+	}
+	$fields = array2csv($fields);
+	print $fields;
+	$page_info_query = 'SELECT '.$fields.' FROM `' . PAGE_TABLE .'`
+		WHERE `id` = '.$id.' LIMIT 1';
+	$page_info_handle = $db->sql_query($page_info_query);
+	if ($db->error[$page_info_handle] === 1) {
+		$debug->add_trace('Failure to read page information',true,'page_get_info');
+		return false;
+	}
+	if ($db->sql_num_rows($page_info_handle) != 1) {
+		$debug->add_trace('Page not found',true,'page_get_info');
+		return false;
+	}
+	$page_info = $db->sql_fetch_assoc($page_info_handle);
+	return $page_info;
+}
+
 function page_add() {
 	// FIXME: Stub
 }
@@ -37,19 +78,11 @@ function page_delete($id) {
 
 	// FIXME: Check for content on page before deleting
 
-	// Check if page exists, and read title for log message.
-	$page_info_query = 'SELECT `title` FROM `' . PAGE_TABLE .'`
-		WHERE `id` = '.$id.' LIMIT 1';
-	$page_info_handle = $db->sql_query($page_info_query);
-	if ($db->error[$page_info_handle] === 1) {
-		$debug->add_trace('Failure to read page information',true,'page_delete');
+	$page_info = page_get_info($id,array('title',' ','hidden'));
+	if (!$page_info) {
+		$debug->add_trace('Failed to retrieve page info',true,'page_delete');
 		return false;
 	}
-	if ($db->sql_num_rows($page_info_handle) != 1) {
-		$debug->add_trace('Page not found',true,'page_delete');
-		return false;
-	}
-	$page_info = $db->sql_fetch_assoc($page_info_handle);
 
 	// Delete page entry
 	$delete_query = 'DELETE FROM `' . PAGE_TABLE . '`
@@ -67,8 +100,32 @@ function page_delete($id) {
 	return true;
 }
 
-function page_hide() {
-	// FIXME: Stub
+function page_hide($id) {
+	global $acl;
+	global $db;
+	global $debug;
+	// Check for permission to execute
+	if (!$acl->check_permission('page_hide')) {
+		$debug->add_trace('Lacking permission to hide pages',true,'page_hide');
+		return false;
+	}
+	// Validate parameters
+	if (!is_int($id)) {
+		$debug->add_trace('ID is not an integer',true,'page_hide');
+		return false;
+	}
+	// Check if page exists
+	$page_info = get_page_info($id,array('title','hidden'));
+	if (!$page_info) {
+		$debug->add_trace('Failed to retrieve page info',true,'page_hide');
+		return false;
+	}
+	// Check if page is already hidden
+	if ($page_info['hidden'] == 1) {
+		$debug->add_trace('Page is already hidden',true,'page_hide');
+		return false;
+	}
+	// FIXME: Stub/incomplete
 }
 
 function page_unhide() {
