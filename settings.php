a4b7951bede68991b417c2a11c53e77dbd6b1780
Stephen Just <stephenjust@users.sf.net>
2009-06-25 20:09:55 +0000

    Fixed call to display_page(), cleaned up code. Still incomplete.

--- settings.php
+++ settings.php
@@ -13,38 +13,37 @@ define('ROOT','./');
 // Load error handling code
 require_once('./functions/error.php');
 // Load database configuration
-if(@ !include_once('./config.php')) {
-    if(@ !include_once('./config.temp.php')) {
-        err_page(0001);
-        }
-    }
+if (@ !include_once('./config.php')) {
+	if (@ !include_once('./config.temp.php')) {
+		err_page(0001);
+	}
+}
 // Check if site is disabled.
-if(@$CONFIG['disabled'] == 1 || @ $CONFIG['not_installed'] == 1) {
-    err_page(11);
-    }
-$NOTIFICATION = NULL;
+if (@$CONFIG['disabled'] == 1 || @ $CONFIG['not_installed'] == 1) {
+	err_page(11);
+}
 
 // Once the database connections are made, include all other necessary files.
-if(!include_once('./include.php')) {
-    err_page(2001); // File not found error.
+if (!include_once('./include.php')) {
+	err_page(2001); // File not found error.
 }
 
 initialize();
 checkuser();
 
-if($site_info['active'] == 0) {
-    err_page(12);
-    }
+if ($site_info['active'] == 0) {
+	err_page(12);
+}
 
 // Load page information.
 $page = new page;
 $page->set_type('settings_main');
-if(file_exists('./install')) {
-    $page->notification .= 'Please delete your ./install directory.<br />';
-}
 // Display the page.
 $page->display_header();
-display_page(NULL,$site_info);
+display_page($site_info);
+if (DEBUG === 1) {
+	$debug->display_traces();
+}
 $page->display_footer();
 
 clean_up();
