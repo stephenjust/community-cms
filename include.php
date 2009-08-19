09d5181b41362ec50c55cae4f6646dbf8cca047d
Stephen Just <stephenjust@users.sf.net>
2009-08-19 03:26:16 +0000

    Forgot a file!

--- include.php
+++ include.php
@@ -23,6 +23,7 @@ if(!include_once(ROOT.'functions/mysql.php')) {
 require(ROOT . 'includes/constants.php');
 require(ROOT . 'includes/db/db.php');
 require(ROOT . 'includes/xml.php');
+require(ROOT . 'includes/template.php');
 require(ROOT . 'functions/login.php');
 require(ROOT . 'includes/page.php');
 if(!include_once(ROOT.'functions/page_class.php')) {
