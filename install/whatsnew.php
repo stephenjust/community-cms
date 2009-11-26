10d45b279448099245cdecfb6a1a1ca3ed43abe5
Stephen Just <stephenjust@users.sf.net>
2009-11-26 01:56:58 +0000

    Updated "What's New" page.

--- install/whatsnew.php
+++ install/whatsnew.php
@@ -17,13 +17,15 @@
 		<div id="main">
 			<h1>What's New?</h1>
 			<ul><li><a href="index.php?page=1">Skip to Installer</a></li></ul>
-			<h2>New in Version 0.7</h2>
+			<h2>New in Version 0.6</h2>
 			<h3>Features</h3>
 			<ul>
 				<li>Pages can now be assigned friendly human-readable names, rather than just numbers</li>
 				<li>Contact entries are now created independant of users</li>
 				<li>Uploaded files can now be assigned attributes</li>
+				<li>It is now really easy to link to a file using TinyMCE</li>
 				<li>News item display settings can be customized now</li>
+				<li>Dates for calendar entries can now be easily selected</li>
 				<li>Multiple news items can be moved, copied, or deleted at once</li>
 				<li>Feedback can now be provided to the developers through a built-in form</li>
 				<li>Users can now be assigned a group to be a member of</li>
@@ -37,10 +39,10 @@
 				<li>Added ability to use custom database tables by editing includes/constants.php</li>
 				<li>Moved a great amount of functionality into classes</li>
 				<li>Separated the database processing layer from the rest of the code</li>
-				<li>Javascript in backend is now compressed by the server before it is sent to the user</li>
 			</ul>
 			<h3>Bug Fixes</h3>
 			<ul>
+				<li>Calendar entries are now listed on the calendar in order of their start time</li>
 				<li>Fixed bug which made it impossible to change a user's password</li>
 				<li>Fixed pagination bug</li>
 				<li>Fixed several SQL Injection vulnerabilities</li>
