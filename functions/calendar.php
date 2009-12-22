8b83dcff771cbdc645aaedd187ac6fccc964ff4f
Stephen Just <stephenjust@users.sf.net>
2009-12-22 02:41:29 +0000

    Cleaned up file

--- functions/calendar.php
+++ functions/calendar.php
@@ -2,7 +2,6 @@
 /**
  * Community CMS
  * $Id$
- *
  * @copyright Copyright (C) 2007-2009 Stephen Just
  * @author stephenjust@users.sourceforge.net
  * @package CommunityCMS.main
@@ -13,40 +12,36 @@ if (@SECURITY != 1) {
 }
 
 /**
- * delete_date - Delete a calendar entry
+ * calendar_settings - Load calendar settings from the database
  * @global object $db
  * @global object $debug
- * @param integer $id
- * @return boolean
+ * @return array
  */
-function delete_date($id) {
+function calendar_settings() {
 	global $db;
 	global $debug;
-	// Validate parameters
-	if (!is_numeric($id)) {
-		$debug->add_trace('Invalid ID given',true,'delete_date()');
-		return false;
+	$query = 'SELECT * FROM `' . CALENDAR_SETTINGS_TABLE . '` LIMIT 1';
+	$handle = $db->sql_query($query);
+	if ($db->error[$handle] === 1) {
+		$debug->add_trace('Failed to check calendar settings',true,'calendar_settings()');
+		// Return default settings
+		return array('default_view' => 'month',
+					'month_show_stime' => 1,
+					'month_show_cat_icons' => 1,
+					'month_day_format' => 1);
 	}
-
-	$read_date_info_query = 'SELECT * FROM ' . CALENDAR_TABLE . '
-		WHERE `id` = '.$id;
-	$read_date_info_handle = $db->sql_query($read_date_info_query);
-	if ($db->error[$read_date_info_handle] === 1) {
-		$debug->add_trace('Failed to read date information. Does it exist?',false,'delete_date()');
-		return false;
-	} else {
-		$del_query = 'DELETE FROM ' . CALENDAR_TABLE . '
-			WHERE `id` = '.$id;
-		$del_handle = $db->sql_query($del_query);
-		$read_date_info = $db->sql_fetch_assoc($read_date_info_handle);
-		if ($db->error[$del_handle] === 1) {
-			return false;
-		} else {
-			log_action('Deleted calendar date \''.$read_date_info['header'].'\'');
-			return true;
-		}
+	if ($db->sql_num_rows($handle) != 1) {
+		$debug->add_trace('No calendar settings. Please repair database.',true,'calendar_settings()');
+		// Return default settings
+		return array('default_view' => 'month',
+					'month_show_stime' => 1,
+					'month_show_cat_icons' => 1,
+					'month_day_format' => 1);
 	}
+	return $db->sql_fetch_assoc($handle);
 }
+
+// ----------------------------------------------------------------------------
 
 /**
  * delete_category - Delete a calendar category entry
@@ -99,25 +94,41 @@ function delete_category($id) {
 	}
 }
 
-function calendar_settings() {
+// ----------------------------------------------------------------------------
+
+/**
+ * delete_date - Delete a calendar entry
+ * @global object $db
+ * @global object $debug
+ * @param integer $id
+ * @return boolean
+ */
+function delete_date($id) {
 	global $db;
 	global $debug;
-	$query = 'SELECT * FROM `' . CALENDAR_SETTINGS_TABLE . '` LIMIT 1';
-	$handle = $db->sql_query($query);
-	if ($db->error[$handle] === 1) {
-		$debug->add_trace('Failed to check calendar settings',true,'calendar_settings()');
-		return array('default_view' => 'month',
-					'month_show_stime' => 1,
-					'month_show_cat_icons' => 1,
-					'month_day_format' => 1);
+	// Validate parameters
+	if (!is_numeric($id)) {
+		$debug->add_trace('Invalid ID given',true,'delete_date()');
+		return false;
 	}
-	if ($db->sql_num_rows($handle) != 1) {
-		$debug->add_trace('No calendar settings. Please repair database.',true,'calendar_settings()');
-		return array('default_view' => 'month',
-					'month_show_stime' => 1,
-					'month_show_cat_icons' => 1,
-					'month_day_format' => 1);
+
+	$read_date_info_query = 'SELECT * FROM ' . CALENDAR_TABLE . '
+		WHERE `id` = '.$id;
+	$read_date_info_handle = $db->sql_query($read_date_info_query);
+	if ($db->error[$read_date_info_handle] === 1) {
+		$debug->add_trace('Failed to read date information. Does it exist?',false,'delete_date()');
+		return false;
+	} else {
+		$del_query = 'DELETE FROM ' . CALENDAR_TABLE . '
+			WHERE `id` = '.$id;
+		$del_handle = $db->sql_query($del_query);
+		$read_date_info = $db->sql_fetch_assoc($read_date_info_handle);
+		if ($db->error[$del_handle] === 1) {
+			return false;
+		} else {
+			log_action('Deleted calendar date \''.$read_date_info['header'].'\'');
+			return true;
+		}
 	}
-	return $db->sql_fetch_assoc($handle);
 }
 ?>
