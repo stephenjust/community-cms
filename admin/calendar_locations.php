778bc832c6f91ffc09ded38f1b1b14b0ff2b176c
Stephen Just <stephenjust@users.sf.net>
2009-12-25 06:38:58 +0000

    Added location manager

---
+++ admin/calendar_locations.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Community CMS
+ * $Id: calendar.php 533 2009-12-25 05:49:37Z stephenjust $
+ *
+ * @copyright Copyright (C) 2007-2009 Stephen Just
+ * @author stephenjust@users.sourceforge.net
+ * @package CommunityCMS.admin
+ */
+// Security Check
+if (@SECURITY != 1 || @ADMIN != 1) {
+	die ('You cannot access this page directly.');
+}
+
+$content = NULL;
+
+include (ROOT.'functions/calendar.php');
+
+// ----------------------------------------------------------------------------
+
+switch ($_GET['action']) {
+	default:
+
+		break;
+
+	case 'new':
+		if (!isset($_POST['location'])) {
+			$content .= 'No location given.<br />'."\n";
+			break;
+		}
+		$check_dupe_query = 'SELECT `value` FROM `'.LOCATION_TABLE.'`
+			WHERE `value` = \''.addslashes($_POST['location']).'\'';
+		$check_dupe_handle = $db->sql_query($check_dupe_query);
+		if ($db->error[$check_dupe_handle] === 1) {
+			$content .= 'Failed to check for duplicate entries.<br />'."\n";
+			break;
+		}
+		if ($db->sql_num_rows($check_dupe_handle) != 0) {
+			$content .= 'The location you are trying to add already exists.<br />'."\n";
+			break;
+		}
+		$new_loc_query = 'INSERT INTO `'.LOCATION_TABLE.'`
+			(`value`) VALUES (\''.addslashes($_POST['location']).'\')';
+		$new_loc_handle = $db->sql_query($new_loc_query);
+		if ($db->error[$new_loc_handle] === 1) {
+			$content .= 'Failed to create new location.<br />'."\n";
+			break;
+		}
+		$content .= 'Successfully created location.<br />'."\n";
+		log_action('Created new location');
+		break;
+
+// ----------------------------------------------------------------------------
+
+	case 'delete':
+		if (!isset($_POST['loc_del'])) {
+			$content .= 'There is no location selected for deletion.<br />'."\n";
+			break;
+		}
+		$del_query = 'DELETE FROM `'.LOCATION_TABLE.'` WHERE `id` = '.(int)$_POST['loc_del'];
+		$del_handle = $db->sql_query($del_query);
+		if ($db->error[$del_handle] === 1) {
+			$content .= 'Failed to delete location.<br />'."\n";
+			break;
+		}
+		$content .= 'Deleted location.<br />'."\n";
+		log_action('Deleted location');
+		break;
+}
+
+// ----------------------------------------------------------------------------
+
+$tab_layout = new tabs;
+$tab_content['manage'] = '<form method="post" action="?module=calendar_locations&action=new">
+New Location: <input type="text" name="location" /><input type="submit" value="Create" /></form>';
+$tab_content['manage'] .= '<form method="post" action="?module=calendar_locations&action=delete">
+<table class="admintable">
+<tr><th width="1px">&nbsp;</th><th>Location:</th></tr>';
+$rowcount = 1;
+$loc_query = 'SELECT * FROM `' . LOCATION_TABLE . '`
+	ORDER BY `value` ASC';
+$loc_handle = $db->sql_query($loc_query);
+$delete_disabled = NULL;
+if ($db->sql_num_rows($loc_handle) == 0) {
+	$tab_content['manage'] .= '<tr><td colspan="2" class="row1">There are no saved locations.</td></tr>';
+	$delete_disabled = ' disabled';
+	$rowcount = 2;
+}
+for ($i = 1; $i <= $db->sql_num_rows($loc_handle); $i++) {
+	$loc = $db->sql_fetch_assoc($loc_handle);
+	$tab_content['manage'] .= '<tr><td class="row'.$rowcount.'">
+		<input type="radio" name="loc_del" value="'.$loc['id'].'" /></td>
+		<td class="row'.$rowcount.'">'.stripslashes($loc['value']).'</td></tr>';
+
+	// Alternate row styles
+	if ($rowcount == 1) {
+		$rowcount = 2;
+	} else {
+		$rowcount = 1;
+	}
+}
+$tab_content['manage'] .= '<tr>
+	<td class="row'.$rowcount.'" colspan="2"><input type="submit" value="Delete" '.$delete_disabled.'/></td></tr>
+</table>
+</form>';
+$tab_layout->add_tab('Manage Locations',$tab_content['manage']);
+
+// ----------------------------------------------------------------------------
+
+$content .= $tab_layout;
+?>
