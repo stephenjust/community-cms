68e466df57878c1c2bd57a1c1120e807facd07b1
Stephen Just <stephenjust@users.sf.net>
2010-01-30 23:57:01 +0000

    Updated editor styles in template too

--- admin.php
+++ admin.php
@@ -106,9 +106,8 @@ function display_admin() {
 			theme : "advanced",
 			skin : "o2k7",
 			plugins : "style,layer,table,save,advhr,advimage,advlist,advlink,comcmslink,cleanupstyles,iespell,insertdatetime,preview,searchreplace,print,contextmenu,paste,directionality,spellchecker,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras",
-			invalid_styles : "margin,line-height",
 			content_css : "scripts/tiny_mce/editor_styles.css",
-			theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,formatselect,fontselect,fontsizeselect,|,help,code",
+			theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,fontselect,fontsizeselect,|,help,code",
 			theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,|,undo,redo,|,link,unlink,comcmslink,image,cleanup,|,forecolor,backcolor",
 			theme_advanced_buttons3 : "tablecontrols,|,sub,sup,|,charmap,iespell,advhr,|,print,|,ltr,rtl,|,fullscreen,|,spellchecker",
 			theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,|,visualchars,nonbreaking,|,insertdate,inserttime,preview,|,hr,removeformat,visualaid",
@@ -117,7 +116,16 @@ function display_admin() {
 			theme_advanced_statusbar_location : "bottom",
 			theme_advanced_fonts:"Arial=arial,helvetica,sans-serif;Arial Narrow=arial narrow,arial,sans-serif;Arial Black=arial black,avant garde;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Tahoma=tahoma,arial,helvetica,sans-serif;Times New Roman=times new roman,times;Verdana=verdana,geneva",
 			theme_advanced_font_sizes:"x-small,small,medium,large,x-large,xx-large",
-			extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]"
+			extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
+			style_formats : [
+				{title : "No spacing", block : "p", classes : "no-spacing"},
+				{title : "Header Styles"},
+				{title : "Heading 1", block : "h2"},
+				{title : "Heading 2", block : "h3"},
+				{title : "Heading 3", block : "h4"},
+				{title : "Table styles"},
+				{title : "Black Border", selector : "table", classes : "black-border"},
+				{title : "No Border", selector : "table", classes : "no-border"}]
 		});
 		</script>';
 	$image_path = $template_page->path.'images/';
