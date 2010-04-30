45db72485b548dcce522b9a1b35d2817b8afaf69
Stephen Just <stephenjust@users.sf.net>
2010-04-30 03:49:46 +0000

    Past Events block works now

--- content_blocks/events_block.php
+++ content_blocks/events_block.php
@@ -12,43 +12,50 @@ switch($event_block->attribute['mode']) {
 	default:
 		$bl_return .= '<strong>ERROR:</strong> Unsupported mode';
 		break;
+		unset($event_block);
+		return $bl_return;
 	case 'upcoming':
-		$upcoming_event_query = 'SELECT * FROM ' . CALENDAR_TABLE . '
+		$event_query = 'SELECT * FROM ' . CALENDAR_TABLE . '
 			WHERE (year = '.date('Y').' AND month = '.date('m').' AND day >= '.date('d').') OR
 			(year = '.date('Y').' AND month > '.date('m').') OR (year > '.date('Y').')
 			ORDER BY year ASC, month ASC, day ASC LIMIT '.$event_block->attribute['num'];
-		$upcoming_event_handle = $db->sql_query($upcoming_event_query);
-		if($db->error[$upcoming_event_handle] === 1) {
-			$bl_return .= 'Failed to load upcoming dates.<br />';
-			return $bl_return;
-		}
-		if($db->sql_num_rows($upcoming_event_handle) == 0) {
-			$bl_return .= '&nbsp;&nbsp;No upcoming dates.<br />';
-			return $bl_return;
-		}
-		$template_upcoming_events = new template;
-		$template_upcoming_events->load_file('mini_events_upcoming');
-		$bl_single_event = $template_upcoming_events->get_range('event');
-		$bl_all_events = NULL;
-		for($i = 1; $i <= $db->sql_num_rows($upcoming_event_handle); $i++) {
-			$upcoming_event = $db->sql_fetch_assoc($upcoming_event_handle);
-			$template_single_event = clone $template_upcoming_events;
-			$template_single_event->template = $bl_single_event;
-			$bl_date = $upcoming_event['day'].'/'.$upcoming_event['month'].'/'.$upcoming_event['year'];
-			$template_single_event->event_date = $bl_date;
-			$template_single_event->event_heading = stripslashes($upcoming_event['header']);
-			$bl_all_events .= $template_single_event;
-			}
-		unset($upcoming_event_handle);
-		unset($upcoming_event);
-		$template_upcoming_events->replace_range('event',$bl_all_events);
-		$bl_return .= $template_upcoming_events;
-		unset($template_upcoming_events);
+		$template_events = new template;
+		$template_events->load_file('mini_events_upcoming');
 		break;
 	case 'past':
-		$bl_return .= 'Past events coming soon.';
+		$event_query = 'SELECT * FROM ' . CALENDAR_TABLE . '
+			WHERE (year = '.date('Y').' AND month = '.date('m').' AND day < '.date('d').') OR
+			(year = '.date('Y').' AND month < '.date('m').') OR (year < '.date('Y').')
+			ORDER BY year DESC, month DESC, day DESC LIMIT '.$event_block->attribute['num'];
+		$template_events = new template;
+		$template_events->load_file('mini_events_past');
 		break;
-	}
+}
+$event_handle = $db->sql_query($event_query);
+if ($db->error[$event_handle] === 1) {
+	$bl_return .= 'Failed to load list of dates.<br />';
+	return $bl_return;
+}
+if($db->sql_num_rows($event_handle) == 0) {
+	$bl_return .= '&nbsp;&nbsp;No dates to list.<br />';
+	return $bl_return;
+}
+$bl_single_event = $template_events->get_range('event');
+$bl_all_events = NULL;
+for($i = 1; $i <= $db->sql_num_rows($event_handle); $i++) {
+	$event = $db->sql_fetch_assoc($event_handle);
+	$template_single_event = clone $template_events;
+	$template_single_event->template = $bl_single_event;
+	$bl_date = $event['day'].'/'.$event['month'].'/'.$event['year'];
+	$template_single_event->event_date = $bl_date;
+	$template_single_event->event_heading = stripslashes($event['header']);
+	$bl_all_events .= $template_single_event;
+}
+unset($event_handle);
+unset($event);
+$template_events->replace_range('event',$bl_all_events);
+$bl_return .= $template_events;
+unset($template_events);
 unset($event_block);
 return $bl_return;
 ?>
