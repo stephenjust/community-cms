24c250306963910dfb653d5487c078cd5e407ade
Stephen Just <stephenjust@users.sf.net>
2010-05-23 20:59:34 +0000

    Use table creation function on config view page

--- admin/config_view.php
+++ admin/config_view.php
@@ -21,7 +21,7 @@ if (!$acl->check_permission('adm_config_view')) {
 
 // ----------------------------------------------------------------------------
 
-$config_query = 'SELECT * FROM `' . CONFIG_TABLE . '`
+$config_query = 'SELECT `config_name`,`config_value` FROM `' . CONFIG_TABLE . '`
 	ORDER BY `config_name` ASC';
 $config_handle = $db->sql_query($config_query);
 if ($db->error[$config_handle] === 1) {
@@ -29,28 +29,15 @@ if ($db->error[$config_handle] === 1) {
 }
 $i = 1;
 $num_entries = $db->sql_num_rows($config_handle);
-$tab_content['view'] = '<table class="admintable">
-	<tr><th>Name</th><th>Value</th></tr>';
-if ($num_entries == 0) {
-	$tab_content['view'] .= '<tr class="row1">
-		<td colspan="2">No configuration values</td>
-		</tr>';
+$config_table_values = array();
+for ($i = 1; $i <= $num_entries; $i++) {
+	$next_row = $db->sql_fetch_row($config_handle);
+	$next_row[0] = stripslashes($next_row[0]);
+	$next_row[1] = htmlentities(stripslashes($next_row[1]));
+	$config_table_values[] = $next_row;
 }
-$rowtype = 1;
-while ($i <= $num_entries) {
-	$config_list = $db->sql_fetch_assoc($config_handle);
-	$tab_content['view'] .= '<tr class="row'.$rowtype.'">
-		<td>'.stripslashes($config_list['config_name']).'</td>
-		<td>'.htmlentities(stripslashes($config_list['config_value'])).'</td>
-		</tr>';
-	if ($rowtype == 1) {
-		$rowtype = 2;
-	} else {
-		$rowtype = 1;
-	}
-	$i++;
-}
-$tab_content['view'] .= '</table>';
+$tab_content['view'] = create_table(array('Name','Value'),
+		$config_table_values);
 $tab_layout = new tabs;
 $tab_layout->add_tab('View Configuration',$tab_content['view']);
 
