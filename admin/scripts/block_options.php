b795a21645932dd2ff20ecd7d3c151f1192b6820
Stephen Just <stephenjust@users.sf.net>
2010-05-29 18:43:24 +0000

    Block info is now stored in and read from XML

--- admin/scripts/block_options.php
+++ admin/scripts/block_options.php
@@ -29,75 +29,70 @@ $referer_directory = dirname($referer);
 if($referer_directory == "") {
 	die('Security breach.');
 }
-/*
-Block File Format:
-
-blockname#parameter('Parameter Name')=type{comma,separated,values}\n
-block2#id('Something ID')=int\n
-block3#
-
-*/
 
 $current_directory = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
 if($current_directory == $referer_directory.'/admin/scripts') {
-	$file_path = ROOT.'content_blocks/blocks.info';
-	$file_handle = fopen($file_path,'r');
-	$file = fread($file_handle,filesize($file_path));
-	fclose($file_handle);
-	$block_entries = explode("\n",$file);
-	$num_entries = count($block_entries);
-	for ($i = 1; $i <= $num_entries; $i++) {
-		$entry = explode('#',$block_entries[$i - 1]);
-		if($entry[0] == $_GET['blocktype']) {
-			$attributes = explode('&',$entry[1]);
-			$num_attributes = count($attributes);
-			$j = 1;
-			if ($num_attributes == 0 || strlen($attributes[0]) < 1) {
-				echo '<input type="hidden" name="attributes" value="" />';
-				echo 'No options.';
-				$num_attributes = 0;
-				}
-			$allattributes = NULL;
-			for ($j = 1; $j <= $num_attributes; $j++) {
-				$atb = explode('=',$attributes[$j - 1]);
-				$temp = explode('(\'',$atb[0]);
-				$attribute_name = $temp[0];
-				$temp = substr($temp[1],0,-2);
-				$attribute_description = $temp;
-				unset($temp);
-				echo $attribute_description.'=';
-				if(preg_match('#\{.+\}#',$atb[1])) {
-					$temp = explode('{',$atb[1]);
-					$attribute_type = $temp[0];
-					$atb[1] = $temp[0];
-					$possible_responses = substr($temp[1],0,-1);
-					}
-				switch($atb[1]) { // $atb[1] = attribute type
-					case 'int':
-						echo '<input type="text" maxlength="9" size="3" name="'.$attribute_name.'" /><br />';
-						break;
-					case 'option':
-						echo '<select name="'.$attribute_name.'">';
-						$possible_responses = explode(',',$possible_responses);
-						for($i = 1; $i <= count($possible_responses); $i++) {
-							echo '<option value="'.$possible_responses[$i - 1].'">'.$possible_responses[$i - 1].'</option>';
-							}
-						echo '</select>';
-						echo '<br />';
-						break;
-					default:
-						echo 'Not supported.<br />';
-						break;
-					} // SWITCH
-				$allattributes .= $attribute_name;
-				if ($j != $num_attributes) {
-					$allattributes .= ',';
-					}
-				} // FOR $j
-			echo '<input type="hidden" name="attributes" value="'.$allattributes.'" />';
-			} // IF $entry[0], $_GET['blocktype']
-		} // FOR $i
-	} else {
-	die('Security breach.');
+	$file_path = ROOT.'includes/blocks.xml';
+	$xmlreader = new XMLReader;
+	$xmlreader->open($file_path);
+	$correct_block = false;
+	$options_list = false;
+	$attribute_count = 0;
+	$attribute_list = array();
+	while ($xmlreader->read()) {
+		if ($xmlreader->name == 'block' && $xmlreader->nodeType == XMLREADER::ELEMENT) {
+			if ($xmlreader->getAttribute('name') == $_GET['blocktype']) {
+				$correct_block = true;
+			}
+		}
+		if ($xmlreader->name == 'block' && $xmlreader->nodeType == XMLREADER::END_ELEMENT) {
+			$correct_block = false;
+		}
+
+		if ($xmlreader->name == 'attribute' && $xmlreader->nodeType == XMLREADER::ELEMENT && $correct_block == true) {
+			echo $xmlreader->getAttribute('label')." \n";
+			$attribute_list[] = $xmlreader->getAttribute('name');
+			switch ($xmlreader->getAttribute('type')) {
+				default:
+					echo 'Not supported.<br />'."\n";
+					break;
+				case 'int':
+					echo '<input type="text" maxlength="9" size="3" name="'
+						.$xmlreader->getAttribute('name').'" /><br />';
+					break;
+				case 'option':
+					echo '<select name="'.$xmlreader->getAttribute('name').'">'."\n";
+					$options_list = true;
+					break;
+			}
+			$attribute_count++;
+		}
+
+		if ($xmlreader->name == 'value'
+				&& $xmlreader->nodeType == XMLREADER::ELEMENT
+				&& $options_list == true
+				&& $correct_block == true) {
+			echo '<option value="'.$xmlreader->readString().'">'.$xmlreader->readString().'</option>';
+		}
+
+		if ($xmlreader->name == 'values'
+				&& $xmlreader->nodeType == XMLREADER::END_ELEMENT
+				&& $options_list == true
+				&& $correct_block == true) {
+			echo '</select><br />'."\n";
+			$options_list = false;
+		}
 	}
+	$xmlreader->close();
+	// FIXME: Initialization should not be necessary.
+	initialize();
+	$attribute_list = array2csv($attribute_list);
+	clean_up();
+	if ($attribute_count == 0) {
+		echo 'No options.<br />';
+	}
+	echo '<input type="hidden" name="attributes" value="'.$attribute_list.'" />';
+} else {
+	die('Security breach.');
+}
 ?>
