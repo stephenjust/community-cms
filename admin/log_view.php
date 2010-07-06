a3b047fc2e39913288cc25de22f348691e70cf1e
Stephen Just <stephenjust@users.sf.net>
2010-07-06 01:10:39 +0000

    Added button to view all logs (before you could only view the last 50 at most)

--- admin/log_view.php
+++ admin/log_view.php
@@ -13,33 +13,49 @@ if (@SECURITY != 1 || @ADMIN != 1) {
 }
 
 $content = NULL;
+$view_all = false;
 
 if (!$acl->check_permission('adm_log_view')) {
 	$content .= 'You do not have the necessary permissions to access this module.';
 	return true;
 }
 
-if ($_GET['action'] == 'delete') {
-	if (!$acl->check_permission('log_clear')) {
-		$content .= 'You are not authorized to clear the log.<br />';
-	} else {
+switch ($_GET['action']) {
+	case 'delete':
+		if (!$acl->check_permission('log_clear')) {
+			$content .= 'You are not authorized to clear the log.<br />';
+			break;
+		}
 		$delete_query = 'TRUNCATE TABLE `' . LOG_TABLE . '`';
 		$delete_handle = $db->sql_query($delete_query);
 		if ($db->error[$delete_handle] === 1) {
 			$content .= 'Failed to clear log messages.<br />';
-		} else {
-			$content .= 'Cleared log messages.<br />'.log_action('Cleared log messages.');
+			break;
 		}
-	}
+		$content .= 'Cleared log messages.<br />'."\n";
+		log_action('Cleared log messages.');
+		break;
+	case 'viewall':
+		$view_all = true;
+		break;
+	default:
+		break;
 }
 
 // ----------------------------------------------------------------------------
 
 $tab_content['view'] = NULL;
-$log_message_query = 'SELECT `log`.`date`,`log`.`action`,
-	`u`.`realname`,`log`.`ip_addr`
-	FROM ' . LOG_TABLE . ' log, ' . USER_TABLE . ' u
-	WHERE log.user_id = u.id ORDER BY log.date DESC LIMIT 50';
+if ($view_all == false) {
+	$log_message_query = 'SELECT `log`.`date`,`log`.`action`,
+		`u`.`realname`,`log`.`ip_addr`
+		FROM ' . LOG_TABLE . ' log, ' . USER_TABLE . ' u
+		WHERE log.user_id = u.id ORDER BY log.date DESC LIMIT 50';
+} else {
+	$log_message_query = 'SELECT `log`.`date`,`log`.`action`,
+		`u`.`realname`,`log`.`ip_addr`
+		FROM ' . LOG_TABLE . ' log, ' . USER_TABLE . ' u
+		WHERE log.user_id = u.id ORDER BY log.date DESC';
+}
 $log_message_handle = $db->sql_query($log_message_query);
 if ($db->error[$log_message_handle] === 1) {
 	$tab_content['view'] .= 'Failed to read log messages.<br />'."\n";
@@ -53,6 +69,10 @@ for ($i = 1; $i <= $db->sql_num_rows($log_message_handle); $i++) {
 }
 $tab_content['view'] .= create_table(array('Date','Action','User','IP'),
 		$table_values);
+if ($view_all == false) {
+	$tab_content['view'] .= '<form method="POST" action="?module=log_view&amp;action=viewall">'."\n".
+		'<input type="submit" value="View All Logs" /></form>'."\n";
+}
 $tab_layout = new tabs;
 $tab_layout->add_tab('View Log Messages',$tab_content['view']);
 
