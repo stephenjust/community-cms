81f4701d09799d2379c3c8b5433b351a1b673ee7
Stephen Just <stephenjust@users.sf.net>
2010-10-09 22:14:00 -0600

    Show news article titles when creating text blocks to be more user friendly

--- communitycms/admin/scripts/block_options.php
+++ communitycms/admin/scripts/block_options.php
@@ -31,65 +31,98 @@ if($referer_directory == "") {
 }
 
 $current_directory = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']);
-if($current_directory == $referer_directory.'/admin/scripts') {
-	$file_path = ROOT.'includes/blocks.xml';
-	$xmlreader = new XMLReader;
-	$xmlreader->open($file_path);
-	$correct_block = false;
-	$options_list = false;
-	$attribute_count = 0;
-	$attribute_list = array();
-	while ($xmlreader->read()) {
-		if ($xmlreader->name == 'block' && $xmlreader->nodeType == XMLREADER::ELEMENT) {
-			if ($xmlreader->getAttribute('name') == $_GET['blocktype']) {
-				$correct_block = true;
-			}
-		}
-		if ($xmlreader->name == 'block' && $xmlreader->nodeType == XMLREADER::END_ELEMENT) {
-			$correct_block = false;
-		}
-
-		if ($xmlreader->name == 'attribute' && $xmlreader->nodeType == XMLREADER::ELEMENT && $correct_block == true) {
-			echo $xmlreader->getAttribute('label')." \n";
-			$attribute_list[] = $xmlreader->getAttribute('name');
-			switch ($xmlreader->getAttribute('type')) {
-				default:
-					echo 'Not supported.<br />'."\n";
-					break;
-				case 'int':
-					echo '<input type="text" maxlength="9" size="3" name="'
-						.$xmlreader->getAttribute('name').'" /><br />';
-					break;
-				case 'option':
-					echo '<select name="'.$xmlreader->getAttribute('name').'">'."\n";
-					$options_list = true;
-					break;
-			}
-			$attribute_count++;
-		}
-
-		if ($xmlreader->name == 'value'
-				&& $xmlreader->nodeType == XMLREADER::ELEMENT
-				&& $options_list == true
-				&& $correct_block == true) {
-			echo '<option value="'.$xmlreader->readString().'">'.$xmlreader->readString().'</option>';
-		}
-
-		if ($xmlreader->name == 'values'
-				&& $xmlreader->nodeType == XMLREADER::END_ELEMENT
-				&& $options_list == true
-				&& $correct_block == true) {
-			echo '</select><br />'."\n";
-			$options_list = false;
-		}
-	}
-	$xmlreader->close();
-	$attribute_list = array2csv($attribute_list);
-	if ($attribute_count == 0) {
-		echo 'No options.<br />';
-	}
-	echo '<input type="hidden" name="attributes" value="'.$attribute_list.'" />';
-} else {
-	die('Security breach.');
+if($current_directory != $referer_directory.'/admin/scripts') {
+	die ('Security Breach');
 }
+
+if ($_GET['blocktype'] == 'text') {
+	initialize();
+	$news_query = 'SELECT `news`.`name`, `news`.`id`, `page`.`title`
+		FROM `'.NEWS_TABLE.'` `news`
+		LEFT JOIN `'.PAGE_TABLE.'` `page`
+		ON `news`.`page` = `page`.`id`
+		ORDER BY `news`.`page` ASC, `news`.`name` ASC';
+	$news_handle = $db->sql_query($news_query);
+	if ($db->error[$news_handle] === 1) {
+		die ('Failed to read news articles.<br />');
+	}
+	$num_articles = $db->sql_num_rows($news_handle);
+	if ($num_articles == 0) {
+		die ('No articles exist.<br />');
+	}
+	echo 'News Article <select name="article_id">'."\n";
+	for ($i = 1; $i <= $num_articles; $i++) {
+		$news_result = $db->sql_fetch_assoc($news_handle);
+		if ($news_result['title'] == NULL) {
+			$news_result['title'] = 'No Page';
+		}
+		echo '<option value="'.$news_result['id'].'">'.$news_result['title'].' - '.$news_result['name'].'</option>'."\n";
+	}
+	echo '</select><br />'."\n";
+	echo 'Show Border <select name="show_border">
+		<option value="yes">Yes</option>
+		<option value="no">No</option>
+		</select><br />';
+	echo '<input type="hidden" name="attributes" value="article_id,show_border" />';
+	clean_up();
+	exit;
+}
+
+$file_path = ROOT.'includes/blocks.xml';
+$xmlreader = new XMLReader;
+$xmlreader->open($file_path);
+$correct_block = false;
+$options_list = false;
+$attribute_count = 0;
+$attribute_list = array();
+while ($xmlreader->read()) {
+	if ($xmlreader->name == 'block' && $xmlreader->nodeType == XMLREADER::ELEMENT) {
+		if ($xmlreader->getAttribute('name') == $_GET['blocktype']) {
+			$correct_block = true;
+		}
+	}
+	if ($xmlreader->name == 'block' && $xmlreader->nodeType == XMLREADER::END_ELEMENT) {
+		$correct_block = false;
+	}
+
+	if ($xmlreader->name == 'attribute' && $xmlreader->nodeType == XMLREADER::ELEMENT && $correct_block == true) {
+		echo $xmlreader->getAttribute('label')." \n";
+		$attribute_list[] = $xmlreader->getAttribute('name');
+		switch ($xmlreader->getAttribute('type')) {
+			default:
+				echo 'Not supported.<br />'."\n";
+				break;
+			case 'int':
+				echo '<input type="text" maxlength="9" size="3" name="'
+					.$xmlreader->getAttribute('name').'" /><br />';
+				break;
+			case 'option':
+				echo '<select name="'.$xmlreader->getAttribute('name').'">'."\n";
+				$options_list = true;
+				break;
+		}
+		$attribute_count++;
+	}
+
+	if ($xmlreader->name == 'value'
+			&& $xmlreader->nodeType == XMLREADER::ELEMENT
+			&& $options_list == true
+			&& $correct_block == true) {
+		echo '<option value="'.$xmlreader->readString().'">'.$xmlreader->readString().'</option>';
+	}
+
+	if ($xmlreader->name == 'values'
+			&& $xmlreader->nodeType == XMLREADER::END_ELEMENT
+			&& $options_list == true
+			&& $correct_block == true) {
+		echo '</select><br />'."\n";
+		$options_list = false;
+	}
+}
+$xmlreader->close();
+$attribute_list = array2csv($attribute_list);
+if ($attribute_count == 0) {
+	echo 'No options.<br />';
+}
+echo '<input type="hidden" name="attributes" value="'.$attribute_list.'" />';
 ?>
