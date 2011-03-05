014b5a5cb64b76092a7e391ba39593ad527309b3
Stephen Just <stephenjust@users.sf.net>
2011-03-04 17:10:48 -0700

    Cleaned up logic

--- communitycms/functions/files.php
+++ communitycms/functions/files.php
@@ -218,146 +218,148 @@ function file_upload($path = "", $contentfile = true, $thumb = false) {
 // ----------------------------------------------------------------------------
 
 /**
- * folder_list - Generate a list of folders
- * @param <type> $directory
- * @param <type> $current
- * @param <type> $type
- * @return <type> 
+ * Generate a list of folders
+ * @param string $directory
+ * @param string $current
+ * @param integer $type
+ * @param string $name
+ * @param string $extra
+ * @return string
  */
 function folder_list($directory = "",$current = "",$type = 0,$name='folder_list',$extra='') {
 	$folder_root = './files/';
-	if (!preg_match('#.#',$directory)) {
-		$folder_open = $folder_root.$directory;
-		$files = scandir($folder_open);
-		$num_files = count($files);
-		$i = 1;
-		$j = 1;
-		$return = NULL;
-		if ($num_files == 0) {
-			$return .= 'There are no files to display in this folder.';
-		}
-		if ($type == 1) { // Start listbox if that is the view mode specified.
-			$return .= '<select name="'.$name.'" id="'.$name.'" '.$extra.'>
+	if (preg_match('#.#',$directory)) {
+		return 'Error retreiving folder list.<br />'."\n";
+	}
+	$folder_open = $folder_root.$directory;
+	$files = scandir($folder_open);
+	$num_files = count($files);
+	$return = NULL;
+	if ($num_files == 0) {
+		$return .= 'There are no files to display in this folder.';
+	}
+	if ($type == 1) { // Start listbox if that is the view mode specified.
+		$return .= '<select name="'.$name.'" id="'.$name.'" '.$extra.'>
 <option value="">Default</option>';
+	}
+	for ($i = 1; $i < $num_files; $i++) {
+		if ($files[$i] == '..' || !is_dir($folder_open.'/'.$files[$i])) {
+			continue;
 		}
-		while ($i < $num_files) {
-			if ($files[$i] != '..' && is_dir($folder_open.'/'.$files[$i])) {
-				if ($type == 0) {
-					$return = $return.$files[$i].'<br />';
-				} elseif ($type == 1) {
-					if ($current == $files[$i]) {
-						$return .= '<option value="'.$files[$i].'" selected>'.$files[$i].'</option>';
-					} else {
-						$return .= '<option value="'.$files[$i].'">'.$files[$i].'</option>';
-					}
-				}
+		if ($type == 0) {
+			$return .= $files[$i].'<br />';
+		} elseif ($type == 1) {
+			if ($current == $files[$i]) {
+				$return .= '<option value="'.$files[$i].'" selected>'.$files[$i].'</option>';
+			} else {
+				$return .= '<option value="'.$files[$i].'">'.$files[$i].'</option>';
 			}
-			$i++;
 		}
-		if ($type == 1) { // End folder listbox if that was the view mode specified.
-			$return = $return.'</select>';
-		}
-	} else {
-		$return = 'Error retrieving folder list.';
+	}
+	if ($type == 1) { // End folder listbox if that was the view mode specified.
+		$return .= '</select>';
 	}
 	return $return;
 }
 
 // ----------------------------------------------------------------------------
 
-// Create a file list
+/**
+ * Generate a list of files
+ * @param string $directory
+ * @param integer $type
+ * @param string $selected
+ * @return string
+ */
 function file_list($directory = "", $type = 0, $selected = "") {
 	$return = NULL;
 	$folder_root = ROOT.'files/';
-	if (preg_match('/[.]/',$directory) == 0) {
-		$folder_open = $folder_root.$directory;
-		$folder_open_short = './files/'.$directory;
-		$files = scandir($folder_open);
-		$num_files = count($files);
-		$i = 1;
-		$j = 1;
-		if ($type == 1) {
-			$return .= '<select name="file_list">';
-		} elseif ($type == 2) {	// If type = 2, display icons for images, and display radio buttons next to each icon. If it is not an image,
-									// do not display it. Add a 'No image' link as well.
-			$return .= '<input type="radio" name="image" value="" checked>No Image<br />';
-			$j++; // Make sure this is displayed even if there's no files.
-		}
-		while ($i < $num_files) {
-			if (!is_dir($folder_open.'/'.$files[$i])) {
-				if ($type == 1) {
-					$return .= '<option value="'.$folder_open_short.'/'.$files[$i].'" />'.$files[$i].'</option>';
-					$j++;
-				} elseif ($type == 2) {
-					if (preg_match('#\.png|\.jpg$#i',$files[$i]) == 1) {
-						$return .= '<div class="admin_image_list_item">';
-						$file_info = get_file_info($folder_open_short.'/'.$files[$i]);
-						if ($folder_open.'/'.$files[$i] == $selected) {
-							$return .= '<input type="radio" name="image" value="'.$folder_open_short.'/'.$files[$i].'" checked /><br /><img src="'.$folder_open.'/'.$files[$i].'" alt="'.$file_info['label'].'" />';
-						} else {
-							$return .= '<input type="radio" name="image" value="'.$folder_open_short.'/'.$files[$i].'" /><br /><img src="'.$folder_open.'/'.$files[$i].'" alt="'.$file_info['label'].'" />';
-						}
-						$return .= '</div>';
-						$j++;
+	if (!preg_match('/[.]/',$directory) == 0) {
+		return 'Error retrieving file list.<br />'."\n";
+	}
+	$folder_open = $folder_root.$directory;
+	$folder_open_short = './files/'.$directory;
+	$files = scandir($folder_open);
+	$num_files = count($files);
+	$j = 0;
+	if ($type == 1) {
+		$return .= '<select name="file_list">';
+	} elseif ($type == 2) {
+		// If type = 2, display icons for images, and display radio buttons
+		// next to each icon. If it is not an image, do not display it. Add
+		// a 'No image' link as well.
+		$return .= '<input type="radio" name="image" value="" checked>No Image<br />';
+		$j++; // Make sure this is displayed even if there's no files.
+	}
+	for ($i = 1; $i < $num_files; $i++) {
+		if (!is_dir($folder_open.'/'.$files[$i])) {
+			if ($type == 1) {
+				$return .= '<option value="'.$folder_open_short.'/'.$files[$i].'" />'.$files[$i].'</option>';
+				$j++;
+			} elseif ($type == 2) {
+				if (preg_match('#\.png|\.jpg$#i',$files[$i]) == 1) {
+					$return .= '<div class="admin_image_list_item">';
+					$file_info = get_file_info($folder_open_short.'/'.$files[$i]);
+					if ($folder_open.'/'.$files[$i] == $selected) {
+						$return .= '<input type="radio" name="image" value="'.$folder_open_short.'/'.$files[$i].'" checked /><br /><img src="'.$folder_open.'/'.$files[$i].'" alt="'.$file_info['label'].'" />';
+					} else {
+						$return .= '<input type="radio" name="image" value="'.$folder_open_short.'/'.$files[$i].'" /><br /><img src="'.$folder_open.'/'.$files[$i].'" alt="'.$file_info['label'].'" />';
 					}
-				} else {
-					$return .= '<a href="'.$folder_open_short.'/'.$files[$i].'">'.$files[$i].'</a><br />';
-					$j++; // Count files that were displayed.
+					$return .= '</div>';
+					$j++;
 				}
+			} else {
+				$return .= '<a href="'.$folder_open_short.'/'.$files[$i].'">'.$files[$i].'</a><br />';
+				$j++; // Count files that were displayed.
 			}
-			$i++;
 		}
-		if ($j == 1) { // If no files were displayed, this will stay at 1.
-			$return = 'There are no files to display.';
-		}
-		if ($type == 1) {
-			$return .= '</select>';
-		}
-	} else {
-		$return = 'Error retrieving file list.';
+	}
+	if ($type == 1) {
+		$return .= '</select>';
+	}
+	// Check if any files were displayed
+	if ($j == 0) {
+		$return = 'There are no files to display.';
 	}
 	return $return;
 }
 
 // ----------------------------------------------------------------------------
 
+/**
+ * Generate a directory and file list that updates through javascript
+ * @param string $directory
+ * @param string $root
+ * @return string
+ */
 function dynamic_file_list($directory = '',$root = ROOT) {
-	//
-	// Folder list portion:
-	//
-
+	// Write folder list
 	$current = $directory;
 	$dropdown_box_options = '<option value="">Default</option>';
 	$folder_root = $root.'files/';
-	if (!preg_match('#./#',$directory)) {
-		$folder_open = $folder_root;
-		$files = scandir($folder_open);
-		$num_files = count($files);
-		$i = 1;
-		$j = 1;
-		$return = NULL;
-		$dropdown_box_options = NULL;
-		while ($i < $num_files) {
-			if ($files[$i] != '..' && is_dir($folder_open.'/'.$files[$i])) {
-				if ($current == $files[$i]) {
-					$dropdown_box_options .= '<option value="'.$files[$i].'" selected>'.$files[$i].'</option>';
-				} else {
-					$dropdown_box_options .= '<option value="'.$files[$i].'">'.$files[$i].'</option>';
-				}
-			}
-			$i++;
+	if (preg_match('#./#',$directory)) {
+		return 'Error retrieving folder list.';
+	}
+	$folder_open = $folder_root;
+	$files = scandir($folder_open);
+	$num_files = count($files);
+	$return = NULL;
+	$dropdown_box_options = NULL;
+	for ($i = 1; $i < $num_files; $i++) {
+		if ($files[$i] == '..' || !is_dir($folder_open.'/'.$files[$i])) {
+			continue;
 		}
-	} else {
-		$return .= 'Error retrieving folder list.';
+		if ($current == $files[$i]) {
+			$dropdown_box_options .= '<option value="'.$files[$i].'" selected>'.$files[$i].'</option>';
+		} else {
+			$dropdown_box_options .= '<option value="'.$files[$i].'">'.$files[$i].'</option>';
+		}
 	}
 	$return .= '<select name="folder_dropdown_box" id="dynamic_folder_dropdown_box" onChange="update_dynamic_file_list(\''.$root.'\')">
 	<option value="">Default</option>'.$dropdown_box_options.'
 	</select><br />';
 
-	//
-	// File list portion:
-	//
-
+	// Generate file list
 	$return .= file_list($directory,1);
 	return $return;
 }
@@ -377,12 +379,12 @@ function get_file_info($file) {
 	$file_info_handle = $db->sql_query($file_info_query);
 	if ($db->error[$file_info_handle] === 1) {
 		$file_info['label'] = 'Could not read information.';
+		return $file_info;
+	}
+	if ($db->sql_num_rows($file_info_handle) != 1) {
+		$file_info['label'] = NULL;
 	} else {
-		if ($db->sql_num_rows($file_info_handle) != 1) {
-			$file_info['label'] = NULL;
-		} else {
-			$file_info = $db->sql_fetch_assoc($file_info_handle);
-		}
+		$file_info = $db->sql_fetch_assoc($file_info_handle);
 	}
 	return $file_info;
 }
