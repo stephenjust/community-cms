75ebb796b7573f6c6603bc5f5c9756b6c4891156
Stephen Just <stephenjust@users.sf.net>
2012-04-28 20:32:02 -0600

    Page message editing moved into function, input validation improved.

--- communitycms/admin/page_message_edit.php
+++ communitycms/admin/page_message_edit.php
@@ -2,13 +2,66 @@
 /**
  * Community CMS
  *
- * @copyright Copyright (C) 2007-2010 Stephen Just
+ * @copyright Copyright (C) 2007-2012 Stephen Just
  * @author stephenjust@users.sourceforge.net
  * @package CommunityCMS.admin
  */
 // Security Check
 if (@SECURITY != 1 || @ADMIN != 1) {
 	die ('You cannot access this page directly.');
+}
+
+/**
+ * Change the content of a page message record
+ * @global acl $acl
+ * @global db $db
+ * @param integer $id
+ * @param integer $page
+ * @param string $content
+ * @param date $start
+ * @param date $end
+ * @param boolean $expire
+ * @throws Exception 
+ */
+function pagemessage_edit($id,$page,$content,$start,$end,$expire) {
+	global $acl;
+	global $db;
+
+	if (!$acl->check_permission('adm_page_message_edit'))
+		throw new Exception('You are not allowed to edit page messages.');
+	
+	// Sanitize inputs
+	$id = (int)$id;
+	if ($id < 1)
+		throw new Exception('An invalid page message id was given.');
+	$page = (int)$page;
+	if ($page < 1)
+		throw new Exception('An invalid page was given.');
+	$content = $db->sql_escape_string($content);
+	if (!preg_match('/[0-9]+\-[0-9]+\-[0-9]+/', $start)
+			|| !preg_match('/[0-9]+\-[0-9]+\-[0-9]+/', $end))
+		throw new Exception('An invalid start or end date was given.');
+	$expire = ($expire === true) ? 1 : 0;
+
+	// Load info of page
+	$page_name_query = 'SELECT `title` FROM `'.PAGE_TABLE.'`
+		WHERE `id` = '.$page.' LIMIT 1';
+	$page_name_handle = $db->sql_query($page_name_query);
+	if ($db->error[$page_name_handle] === 1)
+		throw new Exception('An error occurred while reading page information from the database.');
+	if ($db->sql_num_rows($page_name_handle) != 1)
+		throw new Exception('The page you are trying to edit a page message from does not exist.');
+
+	// Update page message record
+	$edit_article_query = 'UPDATE `'.PAGE_MESSAGE_TABLE."`
+		SET start_date='$start',end_date='$end',end='$expire',text='$content'
+		WHERE message_id = $id";
+	$edit_article = $db->sql_query($edit_article_query);
+	if ($db->error[$edit_article] === 1)
+		throw new Exception('An error occurred when updating the page message record.');
+
+	$page_name = $db->sql_fetch_assoc($page_name_handle);
+	Log::addMessage('Edited page message for page \''.$page_name['title'].'\'');
 }
 
 if (!$acl->check_permission('adm_page_message_edit')) {
@@ -18,14 +71,7 @@ if (!$acl->check_permission('adm_page_message_edit')) {
 
 $content = NULL;
 if ($_GET['action'] == 'edit') {
-	$page_name_query = 'SELECT * FROM ' . PAGE_TABLE . '
-		WHERE id = '.$_POST['page_id'].' LIMIT 1';
-	$page_name_handle = $db->sql_query($page_name_query);
-	if ($db->error[$page_name_handle] === 1) {
-		$content .= 'Failed to read name of current page for log message.<br />';
-	}
-	if ($db->sql_num_rows($page_name_handle) == 1) {
-		$edit_id = (int)$_POST['id'];
+	try {
 		$_POST['start_year'] = (isset($_POST['start_year'])) ? $_POST['start_year'] : 0;
 		$_POST['start_month'] = (isset($_POST['start_month'])) ? $_POST['start_month'] : 0;
 		$_POST['start_day'] = (isset($_POST['start_day'])) ? $_POST['start_day'] : 0;
@@ -35,19 +81,12 @@ if ($_GET['action'] == 'edit') {
 		$start_date = $_POST['start_year'].'-'.$_POST['start_month'].'-'.$_POST['start_day'];
 		$end_date = $_POST['end_year'].'-'.$_POST['end_month'].'-'.$_POST['end_day'];
 		$expire = (isset($_POST['expire'])) ? checkbox($_POST['expire']) : 0;
-		$text = addslashes($_POST['update_content']);
-		$edit_article_query = 'UPDATE ' . PAGE_MESSAGE_TABLE . "
-			SET start_date='$start_date',end_date='$end_date',end='$expire',text='$text' WHERE message_id = $edit_id";
-		$edit_article = $db->sql_query($edit_article_query);
-		if ($db->error[$edit_article] === 1) {
-			$content .= 'Failed to edit page message.<br />';
-		} else {
-			$page_name = $db->sql_fetch_assoc($page_name_handle);
-			$content .= 'Successfully edited page message.<br />';
-			Log::addMessage('Edited page message for page \''.$page_name['title'].'\'');
-		}
-	} else {
-		$content .= 'Failed to find the page which you are trying to edit the message of.';
+		pagemessage_edit($_POST['id'], $_POST['page_id'],
+				$_POST['update_content'], $start_date, $end_date, (boolean)$expire);
+		$content .= 'Successfully edited page message.<br />';
+	}
+	catch (Exception $e) {
+		$content .= '<span class="errormessage">'.$e->getMessage().'</span><br />';
 	}
 } else {
 	if (!isset($_GET['id']) || $_GET['id'] == '') {
