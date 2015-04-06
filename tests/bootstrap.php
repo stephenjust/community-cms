2472b8fa611a58c40a79c943ea2b6c8645c543c4
Stephen Just <stephenjust@gmail.com>
2015-04-05 18:30:58 -0600

    Add some testcases for PaginationComponent.

---
+++ tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Community CMS
+ *
+ * PHP Version 5
+ *
+ * @category  CommunityCMS
+ * @package   CommunityCMS.Tests
+ * @author    Stephen Just <stephenjust@gmail.com>
+ * @copyright 2015 Stephen Just
+ * @license   https://www.apache.org/licenses/LICENSE-2.0 Apache License, 2.0
+ * @link      https://github.com/stephenjust/community-cms
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
